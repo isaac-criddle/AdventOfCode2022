385be20e9379c1e0c1c2a92d99a98db2939b2430
Isaac Criddle <87104088+isaac-criddle@users.noreply.github.com>
2022-12-06 21:07:42 -0700

    start

---
+++ day6.py
@@ -0,0 +1,17 @@
+with open('input6.txt') as f:
+    file = f.readlines()[0]
+
+print(file)
+
+start = []
+for i in range(0, len(file)):
+    if i < 14:
+        start.insert(0, file[i])
+        print(start)
+    else:
+        start.pop()
+        start.insert(0, file[i])
+        print(start)
+        if len(set(start)) == 14:
+            print(i + 1)
+            break
